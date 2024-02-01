d270e67dcce3e40b8052d734821d055b7d1cf060
Khaibar Saadat <khybersaadat93@gmail.com>
2024-02-01 21:00:05 +0100

    Lift 'userInput' state to App Component for effortless sharing with Results Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
+const INITIAL_STATE = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturn: 6,
+  duration: 10,
+};
 
 function App() {
+  const [userInput, setUserInput] = useState(INITIAL_STATE);
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
       <main>
-        <UserInput />
+        <UserInput onChange={handleChange} userInput={userInput} />
+        <Results userInput={userInput} />
       </main>
     </>
   );
